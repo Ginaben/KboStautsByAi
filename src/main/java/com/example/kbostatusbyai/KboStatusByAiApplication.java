2f09cbb9a2cdaf353a33014f9879b4bcd7c7141f
Ginaben <iamgood147@gmail.com>
2024-06-24 14:41:08 +0900

    1

---
+++ src/main/java/com/example/kbostatusbyai/KboStatusByAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kbostatusbyai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KboStatusByAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KboStatusByAiApplication.class, args);
+    }
+
+}
